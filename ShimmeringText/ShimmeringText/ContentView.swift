dfadc89333ffdfd7dba869f727f10ad6d8abd51c
YK <spacex81@gmail.com>
2024-06-21 22:54:15 +0900

    d

---
+++ ShimmeringText/ShimmeringText/ContentView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct Shimmer: ViewModifier {
+    @State private var phase: CGFloat = 0
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                LinearGradient(gradient: Gradient(colors: [.clear, .white.opacity(1.0), .clear]), startPoint: .top, endPoint: .bottom)
+                    .rotationEffect(.degrees(70))
+                    .offset(x: phase * 300 - 150)
+                    .mask(content)
+            )
+            .onAppear {
+                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                    phase = 1
+                }
+            }
+    }
+}
+
+extension View {
+    func shimmering() -> some View {
+        self.modifier(Shimmer())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+            Text("Shimmering Text")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .shimmering()  // Apply the shimmering modifier here
+        }.ignoresSafeArea()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ ShimmeringText/ShimmeringText/ShimmeringTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmeringTextApp.swift
+//  ShimmeringText
+//
+//  Created by jo on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ShimmeringTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
