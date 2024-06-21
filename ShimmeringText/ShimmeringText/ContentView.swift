9ed2f176b2567a182609971f892c9f93db4ad61b
YK <spacex81@gmail.com>
2024-06-21 23:16:06 +0900

    d

--- ShimmeringText/ShimmeringText/ContentView.swift
+++ ShimmeringText/ShimmeringText/ContentView.swift
@@ -2,18 +2,24 @@ import SwiftUI
 
 struct Shimmer: ViewModifier {
     @State private var phase: CGFloat = 0
+//    @State private var shimmerWidth: CGFloat = -150
+    @State private var shimmerWidth: CGFloat = -50
+    
+    let shimmerDuration: CGFloat = 0.7
+
     
     func body(content: Content) -> some View {
         content
             .overlay(
                 LinearGradient(gradient: Gradient(colors: [.clear, .white.opacity(1.0), .clear]), startPoint: .top, endPoint: .bottom)
                     .rotationEffect(.degrees(70))
-                    .offset(x: phase * 300 - 150)
+                    .offset(x: shimmerWidth)
                     .mask(content)
             )
             .onAppear {
-                withAnimation(Animation.linear(duration: 1.3).repeatForever(autoreverses: false)) {
-                    phase = 1
+                withAnimation(.linear(duration: shimmerDuration).repeatForever(autoreverses: false)) {
+//                    phase = 1
+                    shimmerWidth = -shimmerWidth
                 }
             }
     }
@@ -29,11 +35,11 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.black
-            Text("Shimmering Text")
+            Text("AAA")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(.gray)
-                .shimmering()  // Apply the shimmering modifier here
+                .shimmering()
         }.ignoresSafeArea()
     }
 }
